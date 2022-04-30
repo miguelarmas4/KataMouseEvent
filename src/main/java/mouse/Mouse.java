4a26608771f23f2546e938cf451aa00d06153b35
Miguel Armas <miguel.a.armas@gmail.com>
2022-04-30 08:28:49 +0100

    Agregando la interfase, el enum y el resto del código del ejercicio base

--- src/main/java/mouse/Mouse.java
+++ src/main/java/mouse/Mouse.java
@@ -1,4 +1,32 @@
 package mouse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mouse {
+    private List<MouseEventListener> listeners= new ArrayList<>();
+    private final long timeWindowInMillisecondsForDoubleClick = 500;
+
+    public void pressLeftButton(long currentTimeInMilliseconds) {
+        /*... implement this method ...*/
+    }
+
+    public void releaseLeftButton(long currentTimeInMilliseconds) {
+        /*... implement this method ...*/
+    }
+
+    public void move(MousePointerCoordinates from, MousePointerCoordinates to, long
+            currentTimeInMilliseconds) {
+        /*... implement this method ...*/
+    }
+
+    public void subscribe(MouseEventListener listener) {
+        listeners.add(listener);
+    }
+
+    private void notifySubscribers(MouseEventType eventType) {
+        listeners.forEach(listener -> listener.handleMouseEvent(eventType));
+    }
+
+
 }
