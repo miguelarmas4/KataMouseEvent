cc24ed2e8ccf72168cf5e136ec4f042f02cd2e41
Miguel Armas <miguel.a.armas@gmail.com>
2022-04-30 22:06:59 +0100

    Test Click

--- src/main/java/mouse/Mouse.java
+++ src/main/java/mouse/Mouse.java
@@ -1,20 +1,64 @@
 package mouse;
 
 import java.util.ArrayList;
+import java.util.EventListener;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class Mouse {
     private List<MouseEventListener> listeners= new ArrayList<>();
     private final long timeWindowInMillisecondsForDoubleClick = 500;
+    public static long clickTimeWindow = 500;
+    private static MouseEventType eventToTrigger;
+
+    private long timePressed;
+    private boolean pressButton;
+    private boolean isWaitingToTriggerSingleClick;
 
     public void pressLeftButton(long currentTimeInMilliseconds) {
         /*... implement this method ...*/
+        timePressed = timeWindowInMillisecondsForDoubleClick;
+        pressButton = true;
     }
 
     public void releaseLeftButton(long currentTimeInMilliseconds) {
         /*... implement this method ...*/
+        if (pressButton) {
+            pressButton = false;
+            handleClickEvent();
+        }
     }
 
+    private void handleClickEvent() {
+        calculateTypeOfClickEvent();
+        afterTheClickTimeWindow(() -> {
+            boolean isClick = isFinallyAClickEvent();
+            resetClickState();
+            if (isClick) {
+                notifySubscribers();
+            }
+        });
+    }
+
+    private void resetClickState() {
+        isWaitingToTriggerSingleClick = false;
+    }
+
+    private boolean isFinallyAClickEvent() {
+        return isWaitingToTriggerSingleClick;
+    }
+
+    private void calculateTypeOfClickEvent() {
+        if (!isWaitingToTriggerSingleClick) {
+            isWaitingToTriggerSingleClick = true;
+            eventToTrigger = MouseEventType.SingleClick;
+        }
+    }
+
+    private void afterTheClickTimeWindow(Runnable runnable) {
+        CompletableFuture.delayedExecutor(clickTimeWindow, TimeUnit.MILLISECONDS).execute(runnable);
+    }
     public void move(MousePointerCoordinates from, MousePointerCoordinates to, long
             currentTimeInMilliseconds) {
         /*... implement this method ...*/
@@ -24,8 +68,8 @@ public class Mouse {
         listeners.add(listener);
     }
 
-    private void notifySubscribers(MouseEventType eventType) {
-        listeners.forEach(listener -> listener.handleMouseEvent(eventType));
+    private void notifySubscribers() {
+        listeners.forEach(listener -> listener.handleMouseEvent(eventToTrigger));
     }
 
 
--- src/test/java/MouseEventTests.java
+++ src/test/java/MouseEventTests.java
@@ -1,2 +1,55 @@
+import mouse.Mouse;
+import mouse.MouseEventListener;
+import mouse.MouseEventType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 public class MouseEventTests {
+
+    private Mouse mouse;
+    private SpyEventListener listener;
+
+    class SpyEventListener implements MouseEventListener {
+
+        public MouseEventType receivedEventType;
+        public boolean wasEventTriggered;
+        public int eventCount;
+
+        @Override
+        public void handleMouseEvent(MouseEventType eventType) {
+            this.receivedEventType = eventType;
+            wasEventTriggered = true;
+            eventCount++;
+        }
+    }
+
+    @Before
+    public void initMouse() {
+        mouse = new Mouse();
+        listener = new SpyEventListener();
+        mouse.subscribe(listener);
+    }
+
+    /*
+    press left mouse button
+    release left mouse button
+    move mouse
+     */
+
+    @Test
+    public void single_click_means_pressing_and_releasing_mouse_button() throws InterruptedException {
+
+        mouse.pressLeftButton(System.currentTimeMillis());
+        mouse.releaseLeftButton(System.currentTimeMillis() + 10);
+
+        delaySimulatingHumanUser();
+        assertThat(listener.receivedEventType).isEqualTo(MouseEventType.SingleClick);
+
+    }
+
+    private void delaySimulatingHumanUser() throws InterruptedException {
+        Thread.sleep(Mouse.clickTimeWindow + 100);
+    }
 }
